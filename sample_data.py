98dfba4a66a8abd0f7bdcb811ec9d88819110746
VedhikaM <135841934+VedhikaM@users.noreply.github.com>
2026-04-26 19:14:40 +0530

    sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,7 @@
+inputs = [
+    "Looking for pricing details",
+    "Need help with product features",
+    "Interested in demo",
+    "Want to know subscription cost",
+    "Facing issue with login"
+]
